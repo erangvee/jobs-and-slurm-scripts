fbf05dde8c7d6aec26ebf1dd8c6e7b09e8ae54bd
Aira <villapando.aira@gmail.com>
2025-08-29 07:26:02 +0800

    fix issues

--- job_array.sh
+++ job_array.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
+
 #SBATCH --job-name=task_array
-#SBATCH --output=logs/output_%A_%a.out
-#SBATCH --error=logs/error_%A_%a.err
-#SBATCH --array=0-9
+#SBATCH --account=your_account_here
+#SBATCH --qos=your_qos_here
 #SBATCH --ntasks=1
-#SBATCH --time=00:01:00
 #SBATCH --mem=512M
-#SBATCH --partition=<partition>
-#SBATCH --qos=<qos>
+#SBATCH --array=0-4
+#SBATCH --time=00:03:00
+#SBATCH --output=./output/%x_%A_%a.out
+#SBATCH --error=./output/%x_%A_%a.err
 #SBATCH --requeue
 
-spack env activate scratch/spack/hpc-tests
+mkdir -p ./output
+
+spack env activate /scratch/user.test/spack/spack_env
 spack load miniconda3
 
-source activate hpc-tests
-python task.py
+source activate conda_env
+python job_array.py ${SLURM_ARRAY_TASK_ID}
